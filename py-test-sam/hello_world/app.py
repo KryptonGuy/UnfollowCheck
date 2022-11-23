c39f7395bc314f29e294fa505460f70e4c048178
KryptonGuy <yash.sant.orignal@gmail.com>
2022-11-23 23:12:15 +0530

    https

--- py-test-sam/hello_world/app.py
+++ py-test-sam/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "url": "http://kryptonguy.me",
+            "url": "https://kryptonguy.me",
             # "location": ip.text.replace("\n", "")
         }),
     }
